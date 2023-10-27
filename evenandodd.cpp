3a46fb28dab929131078d0fcdc856c1fd8f29a44
SHREYA NARAYAN <140142783+shreyyeah@users.noreply.github.com>
2023-10-27 23:17:20 +0530

    Add files via upload

---
+++ evenandodd.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main()
+{
+   
+    using namespace std;
+    int n;
+    cout<<"Enter number:";
+    cin>>n;
+    if(n%2==0)
+    {cout<<"It is an even number";}
+    else (n%2 !=0);
+    {cout<<"It is an odd number ";}
+    
+}
